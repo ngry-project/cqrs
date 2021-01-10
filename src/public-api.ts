6c7e015e22585d7d35c4a510cd05285fac745284
achugaev <achugaev93@gmail.com>
2021-01-10 16:43:17 +0200

    refactor: exports

--- src/public-api.ts
+++ src/public-api.ts
@@ -13,8 +13,8 @@ export * from './lib/query/query.interface';
 export * from './lib/query/query-bus';
 export * from './lib/query/query-handler';
 
+export * from './lib/operator/of-type';
+
 export * from './lib/saga/saga';
 
 export * from './lib/cqrs.module';
-
-export * from './lib/operator/of-type';
